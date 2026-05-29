89a17b98c312b171fb8c0cacc4334e92a2d4b46a
claraanjelitakd <claraanjelitakd@gmail.com>
2026-05-29 23:55:40 +0700

    F

--- config/cors.php
+++ config/cors.php
@@ -6,7 +6,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'], // bisa diganti origin frontend
+    'allowed_origins' => [
+        'http://localhost:8001',
+        'http://127.0.0.1:8001',
+    ],  // bisa diganti origin frontend
 
     'allowed_origins_patterns' => [],
 
