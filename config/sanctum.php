bd1048d304cfca95d77dbb642061398cb861bdf3
claraanjelitakd <claraanjelitakd@gmail.com>
2026-05-30 00:46:35 +0700

    F

--- config/sanctum.php
+++ config/sanctum.php
@@ -17,9 +17,8 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,127.0.0.1:8001,::1,backend-linguedu-server-production.up.railway.app',
         Sanctum::currentApplicationUrlWithPort(),
-        // Sanctum::currentRequestHost(),
     ))),
 
     /*
